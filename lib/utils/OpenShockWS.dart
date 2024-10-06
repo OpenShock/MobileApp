d625469f796dd6f60e5b1d3aba4d80df7514a181
ImmaCodePrivate <private@aubury.me>
2024-10-06 16:33:25 +0100

    Correcting headers, adding controlSignal

--- lib/utils/OpenShockWS.dart
+++ lib/utils/OpenShockWS.dart
@@ -1,5 +1,21 @@
+import 'dart:io';
+
 import 'package:open_shock/main.dart';
 import 'package:signalr_core/signalr_core.dart';
+import 'package:http/http.dart' as http;
+
+class _HttpClient extends http.BaseClient {
+  final _httpClient = http.Client();
+  final Map<String, String> defaultHeaders;
+
+  _HttpClient({required this.defaultHeaders});
+
+  @override
+  Future<http.StreamedResponse> send(http.BaseRequest request) {
+    request.headers.addAll(defaultHeaders);
+    return _httpClient.send(request);
+  }
+}
 
 class OpenshockWS {
   String session_key, api_host;
@@ -12,15 +28,18 @@ class OpenshockWS {
   // Start the connection
   Future<void> startConnection() async {
     try {
+      final httpClient = _HttpClient(defaultHeaders: {
+        'OpenShockSession': this.session_key,
+        'User-Agent': GetUserAgent(),
+      });
+
       connection = HubConnectionBuilder()
+          .withAutomaticReconnect()
           .withUrl(
               api_host + '/1/hubs/user',
               HttpConnectionOptions(
                   logging: (level, message) => print(message),
-                  customHeaders: {
-                    'Cookie': "openShockSession=" + this.session_key,
-                    'User-Agent': GetUserAgent(),
-                  },
+                  client: httpClient,
                   skipNegotiation: true,
                   transport: HttpTransportType.webSockets,
                   logMessageContent: true))
@@ -61,5 +80,28 @@ class OpenshockWS {
     } else {
       print('Connection is not established.');
     }
+  }
+
+  Future<bool> sendControlSignal(
+      String id, int intensity, int duration, int action) {
+    var data = <String, dynamic>{
+      "Id": id,
+      "Type": action,
+      "Duration": duration,
+      "Intensity": intensity,
+    };
+
+    try {
+      // Wrap the Map in a List
+      connection!.send(methodName: 'ControlV2', args: [
+        [data],
+        null
+      ]);
+      print('Sent control Signal ${data}');
+    } catch (e) {
+      return Future.value(false);
+    }
+
+    return Future.value(true);
   }
 }
